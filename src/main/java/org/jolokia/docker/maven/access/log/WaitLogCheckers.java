8af4b9852a7e6f4a5d10b7d38bd83856a072d28e
Mykola Gurov <ngurov@gmail.com>
2015-09-11 18:42:08 +0200

    something like this i guess

--- src/main/java/org/jolokia/docker/maven/access/log/WaitLogCheckers.java
+++ src/main/java/org/jolokia/docker/maven/access/log/WaitLogCheckers.java
@@ -1,69 +1,139 @@
 package org.jolokia.docker.maven.access.log;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import org.jolokia.docker.maven.access.DockerAccess;
 import org.jolokia.docker.maven.util.Logger;
 import org.jolokia.docker.maven.util.Timestamp;
 import org.jolokia.docker.maven.util.WaitUtil;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class WaitLogCheckers {
-    public static Collection<WaitUtil.WaitChecker> getLogWaitChecker(final String logPattern,
-                                                                     final String fail,
-                                                                     final String containerId,
-                                                                     final DockerAccess docker,
-                                                                     final Logger log) {
-        return Collections.<WaitUtil.WaitChecker>singleton(new WaitUtil.WaitChecker() {
+    public static Collection<? extends WaitUtil.WaitChecker> getLogWaitChecker(final String logPattern,
+                                                                               final String fail,
+                                                                               final String containerId,
+                                                                               final DockerAccess docker,
+                                                                               final Logger log) {
 
-            private LogGetHandle logHandle;
-            private volatile WaitUtil.WaitStatus detected = WaitUtil.WaitStatus.unknown;
+        final List<RegexpWaitLogChecker> checkers = new ArrayList<>();
 
+        if (null != logPattern) {
+            checkers.add(new RegexpWaitLogChecker(Pattern.compile(logPattern), log, WaitLogCheckerType.ok));
+        }
+
+        if (null != fail) {
+            checkers.add(new RegexpWaitLogChecker(Pattern.compile(fail), log, WaitLogCheckerType.fail));
+        }
+
+        if (checkers.isEmpty()) {
+            return checkers;
+        }
+
+        final Supplier<LogGetHandle> logHandleSupplier = new Supplier<LogGetHandle>() {
             @Override
-            public WaitUtil.WaitStatus check() {
-                if (null == logHandle) {
-                    final Predicate<CharSequence> ok = null == logPattern ? Predicates.<CharSequence>alwaysFalse() : Predicates.containsPattern(logPattern);
-                    final Predicate<CharSequence> ko = null == fail ? Predicates.<CharSequence>alwaysFalse() : Predicates.containsPattern(fail);
-                    logHandle = docker.getLogAsync(containerId, new LogCallback() {
-                        @Override
-                        public void log(int type, Timestamp timestamp, String txt) throws DoneException {
-                            if (ok.apply(txt)) {
-                                detected = WaitUtil.WaitStatus.positive;
-                                throw new DoneException();
-                            }
-                            if (ko.apply(txt)) {
-                                detected = WaitUtil.WaitStatus.negative;
-                                throw new DoneException();
-                            }
+            public LogGetHandle get() {
+                return docker.getLogAsync(containerId, new LogCallback() {
+                    @Override
+                    public void log(int type, Timestamp timestamp, String txt) throws DoneException {
+                        for (RegexpWaitLogChecker checker : checkers) {
+                            checker.log(type, timestamp, txt);
                         }
+                    }
 
-                        @Override
-                        public void error(String error) {
-                            log.error(error);
-                        }
-                    });
-                }
-                return detected;
+                    @Override
+                    public void error(String error) {
+                        log.error(error);
+                    }
+                });
             }
+        };
 
+        final Supplier<LogGetHandle> cachedLogHandleSupplier = Suppliers.memoize(logHandleSupplier);
+
+        for (RegexpWaitLogChecker checker : checkers) {
+            checker.setLogHandleSupplier(cachedLogHandleSupplier);
+        }
+
+        return Collections.unmodifiableCollection(checkers);
+    }
+
+    private enum WaitLogCheckerType {
+        ok {
             @Override
-            public void cleanUp() {
-                if (logHandle != null) {
-                    logHandle.finish();
-                }
+            public WaitUtil.WaitStatus toStatus() {
+                return WaitUtil.WaitStatus.positive;
             }
-
+        },
+        fail {
             @Override
-            public boolean isRequired() {
-                return logPattern != null;
+            public WaitUtil.WaitStatus toStatus() {
+                return WaitUtil.WaitStatus.negative;
             }
+        };
 
-            @Override
-            public String toString() {
-                return "on log out '" + logPattern + "'";
+        public abstract WaitUtil.WaitStatus toStatus();
+    }
+
+    private static class RegexpWaitLogChecker implements WaitUtil.WaitChecker, LogCallback {
+
+        private final Pattern pattern;
+
+        private final Logger log;
+
+        private final WaitLogCheckerType whenDetected;
+
+        private volatile WaitUtil.WaitStatus detected = WaitUtil.WaitStatus.unknown;
+
+        private Supplier<LogGetHandle> logHandleSupplier;
+
+        private boolean tracing = false;
+
+        public RegexpWaitLogChecker(Pattern pattern, Logger log, WaitLogCheckerType whenDetected) {
+            this.pattern = pattern;
+            this.log = log;
+            this.whenDetected = whenDetected;
+        }
+
+        @Override
+        public void log(int type, Timestamp timestamp, String txt) throws DoneException {
+            if (pattern.matcher(txt).find()) {
+                detected = whenDetected.toStatus();
+                throw new DoneException();
             }
-        });
+        }
+
+        @Override
+        public void error(String error) {
+            log.error(error);
+        }
+
+        @Override
+        public WaitUtil.WaitStatus check() {
+            if (!tracing) {
+                tracing = null != logHandleSupplier.get();
+            }
+            return detected;
+        }
+
+        @Override
+        public void cleanUp() {
+            if (tracing) {
+                logHandleSupplier.get().finish(); //TODO: protect against double reset? Might not be needed.
+            }
+        }
+
+        @Override
+        public boolean isRequired() {
+            return WaitLogCheckerType.ok == whenDetected;
+        }
+
+        public void setLogHandleSupplier(Supplier<LogGetHandle> logHandleSupplier) {
+            this.logHandleSupplier = logHandleSupplier;
+        }
     }
 }
